442c510e307748b2912e1b7e8491ed68beabd6a1
fyhao <fyhao1@gmail.com>
2019-07-21 16:35:08 +0800

    xcodebuild relesae

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,11 +12,12 @@ cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provis
 echo "*********************"
 echo "*     Archiving     *"
 echo "*********************"
-xcrun xcodebuild -workspace app/Jasonette.xcworkspace -scheme Jasonette -archivePath $ARCHIVE_NAME.xcarchive archive DEVELOPMENT_TEAM=C4F7EVGZVS > log1.txt
+#xcrun xcodebuild -workspace app/Jasonette.xcworkspace -scheme Jasonette -archivePath $ARCHIVE_NAME.xcarchive archive DEVELOPMENT_TEAM="C4F7EVGZVS" CODE_SIGN_IDENTITY="iPhone Distribution: Khor Yong Hao (C4F7EVGZVS)" PROVISIONING_PROFILE="b3c27c8a-4dda-4202-8f53-a752e20add13" > log1.txt
+xcrun xcodebuild -configuration Release build -project app/Jasonette.xcodeproj -target Jasonette ARCHS=armv7 arm64 VALID_ARCHS=armv7 arm64 -sdk iphoneos CODE_SIGN_IDENTITY="iPhone Distribution: Khor Yong Hao (C4F7EVGZVS)" PROVISIONING_PROFILE="b3c27c8a-4dda-4202-8f53-a752e20add13" DEVELOPMENT_TEAM="C4F7EVGZVS" > log1.txt
 echo "**********************"
 echo "*     Exporting      *"
 echo "**********************"
 xcrun xcodebuild -exportArchive -archivePath $ARCHIVE_NAME.xcarchive -exportPath . -exportOptionsPlist ExportOptions.plist > log2.txt
 echo "*****Upload Log*******"
-tail -n 20 log1.txt > log1_100.txt
+tail -n 15 log1.txt > log1_100.txt
 curl -F "fileUploaded=@log1_100.txt" http://iplacesquare.fyhao-apps.com/fileupload
